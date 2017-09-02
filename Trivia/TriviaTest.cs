8ea7c6c125c814c6800686ab48985cc352aa5164
arttsai <arttsai@gmail.com>
2017-09-02 15:28:10 +0800

    installed nunit (with framework 2.6.4) and  ApprovalTests

---
+++ Trivia/TriviaTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+
+namespace Trivia
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    class TriviaTest
+    {
+        [Test]
+        public void DeterministicTest()
+        {
+//            Assert.AreEqual(1,1);
+        }
+    }
+}
